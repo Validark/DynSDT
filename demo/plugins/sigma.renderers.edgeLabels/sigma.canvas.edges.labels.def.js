1e7d1b16dfce067f64e2d53139263f43011fb805
Validark <Validarkness@gmail.com>
2022-06-20 16:23:34 -0500

    Update sigma.canvas.edges.labels.def.js

--- demo/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.def.js
+++ demo/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.def.js
@@ -240,7 +240,7 @@
 	].join(' ');
 	if (!edge.doNotBack) {
 		const numBackingChars = Math.ceil(context.measureText(label).width / context.measureText("\u{2588}").width)
-		console.log({ numBackingChars })
+		console.log(context)
 		context.fillText("\u{2588}".repeat(numBackingChars), fill_x, fill_y);
 	}
 	context.restore()
